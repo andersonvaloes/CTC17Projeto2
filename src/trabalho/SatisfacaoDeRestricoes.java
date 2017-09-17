514086b9cdc09ed371a32afd767026c4b2b09d77
Anderson <andersonvaloes@gmail.com>
2017-09-17 12:47:01 -0300

    Iniciada a questão 2.2

---
+++ src/trabalho/SatisfacaoDeRestricoes.java
@@ -0,0 +1,9 @@
+package trabalho;
+
+public class SatisfacaoDeRestricoes {
+	int[] casas = {1, 2, 3, 4, 5};
+	String[] core = {"Vermelho", "Amarela", "Azul", "Laranja", "Verde"};
+	String[]  nacionalidade = {"Ingles", "Espanhol", "Noruegues", "Ucraniano", "Japones"};
+	String[] bebida = {"Suco", "Cha", "Cafe", "Leite", "Agua"};
+	String[] animal = {"Zebra", "Cachorro", "Raposa", "Caramujo", "Cavalo"};
+}
